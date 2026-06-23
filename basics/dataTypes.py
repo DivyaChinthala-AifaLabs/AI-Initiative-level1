a22999051541fa7d35e8c0e69780dac0e14195c0
DivyaChinthala-AifaLabs <Divya.C@aifalabs.com>
2026-06-23 14:31:22 +0530

    learn about venv, pip & pyproject.toml

--- basics/dataTypes.py
+++ basics/dataTypes.py
@@ -13,3 +13,18 @@
 # bytearray
 # set
 
+# venv - create a separate environment for the python project
+# it allows us to install packages and maintain separately for each project without installing them globally
+# python -m venv .venv
+# .venv\Scripts\activate.bat
+# deactivate - this command is used to exit from virtual environment
+
+# pip
+# it is a python package manager similar to npm in javascript
+# Using pip, we can install, upgrade, uninstall, and manage Python packages.
+# cmd - pip install <package-name>
+
+# pyproject.toml
+# it is similar to package.json file in NodeJs
+# it consists of name, version, dependencies, tools etc configuration details related to python proj
+# we don't need requirements.txt, if we have pyproject.toml file
